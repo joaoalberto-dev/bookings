002eaa5730b40eff1ec68c6ce1f5e8637be7f166
João Alberto <joaoalberto.contato@gmail.com>
2026-01-09 16:00:31 -0300

    chore(setup): creating reusable aliases for code and tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types="vitest/config" />
+import path from "node:path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -11,6 +13,12 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: [
+      { find: "@/tests", replacement: path.resolve(__dirname, "./tests") },
+      { find: "@", replacement: path.resolve(__dirname, "./src") },
+    ],
+  },
   test: {
     globals: true,
     environment: "jsdom",
