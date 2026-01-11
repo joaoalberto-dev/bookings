d95d16e586fce905e9c85b51681c50096a4f195d
João Alberto <joaoalberto.contato@gmail.com>
2026-01-11 13:52:31 -0300

    chore(tests): adding test helpers for testing fetch and errors

---
+++ tests/helpers.ts
@@ -0,0 +1,42 @@
+import { vi } from "vitest";
+
+export const mockFetch = (data: unknown) => {
+  vi.mocked(fetch).mockResolvedValueOnce({
+    ok: true,
+    json: async () => await data,
+  } as Response);
+};
+
+export const mockFetchWithError = (status = 500) => {
+  vi.mocked(fetch).mockResolvedValueOnce({
+    ok: false,
+    status: status,
+  } as Response);
+};
+
+export const mockFetchErrorThrow = (error?: Error) => {
+  vi.mocked(fetch).mockRejectedValueOnce(error);
+};
+
+export const mockFetchWithResolve = () => {
+  let resolve: (response: Partial<Response>) => void;
+
+  const pendingPromise = new Promise<Response>((res) => {
+    resolve = res as typeof resolve;
+  });
+
+  vi.mocked(fetch).mockReturnValueOnce(pendingPromise);
+
+  return {
+    resolve: (
+      response: Partial<Response> = {
+        ok: true,
+        json: async () => {
+          // Noop
+        },
+      },
+    ) => {
+      resolve(response as Response);
+    },
+  };
+};
