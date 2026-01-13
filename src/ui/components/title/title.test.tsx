6fc712de44ad5d7ee49902f888bfd8cb103bffe4
João Alberto <joaoalberto.contato@gmail.com>
2026-01-13 19:52:45 -0300

    chore(tests): improve title tests coverage

--- src/ui/components/title/title.test.tsx
+++ src/ui/components/title/title.test.tsx
@@ -12,15 +12,23 @@ describe("Title", () => {
   });
 
   test("should return null when null children is provided", () => {
-    render(<Title children={null} />);
+    const { container } = render(<Title>{null}</Title>);
 
     expect(screen.queryByRole("heading")).not.toBeInTheDocument();
+    expect(container.firstChild).toBeNull();
   });
 
-  test("should return null when no children is provided", () => {
-    // @ts-expect-error -- Tests only
-    render(<Title />);
+  test("should return null when undefined children is provided", () => {
+    const { container } = render(<Title>{undefined}</Title>);
 
     expect(screen.queryByRole("heading")).not.toBeInTheDocument();
+    expect(container.firstChild).toBeNull();
+  });
+
+  test("should return null when empty string is provided", () => {
+    const { container } = render(<Title>{""}</Title>);
+
+    expect(screen.queryByRole("heading")).not.toBeInTheDocument();
+    expect(container.firstChild).toBeNull();
   });
 });
