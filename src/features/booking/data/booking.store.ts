cd4a70d3a29589e45133a34b6b38b15f65067f76
João Alberto <joaoalberto.contato@gmail.com>
2026-01-15 16:42:03 -0300

    chore(bookings): add support for current booking, update current booking and creating bookings

--- src/features/booking/data/booking.store.ts
+++ src/features/booking/data/booking.store.ts
@@ -4,12 +4,31 @@ import { type Booking } from "./booking.types";
 
 interface BookingState {
   bookings: Booking[];
+  currentBooking: Partial<Booking> | null;
   error: string | null;
+  updateCurrentBooking: (booking: Partial<Booking> | null) => void;
+  createBooking: (booking: Booking) => void;
 }
 
-const useBookingStore = create<BookingState>(() => ({
+const useBookingStore = create<BookingState>((set, get) => ({
   bookings: [],
+  currentBooking: null,
   error: null,
+  updateCurrentBooking(inputBooking) {
+    set({
+      currentBooking: {
+        ...get().currentBooking,
+        ...inputBooking,
+      },
+    });
+  },
+  createBooking(booking: Booking) {
+    set({
+      bookings: [...get().bookings, booking],
+    });
+  },
 }));
 
 export const useBookings = () => useBookingStore((store) => store.bookings);
+export const useCurrentBooking = () => useBookingStore((store) => store.currentBooking);
+export const useUpdateBooking = () => useBookingStore((store) => store.updateCurrentBooking);
