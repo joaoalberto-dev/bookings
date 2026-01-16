81d3b9cec599c7410dc6c584eafadc1ddef87324
João Alberto <joaoalberto.contato@gmail.com>
2026-01-16 18:43:21 -0300

    chore(app): rendering components in entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
 import "./ui/styles/global.css";
 
+import { useState } from "react";
+
+import { type Property } from "@/features/properties/data/properties.types";
+import { PropertiesList } from "@/features/properties/ui/property-list/properties-list";
+import { Header } from "@/ui/components/header/header";
+
+import { useClearCurrentBooking, useUpdateCurrrentBooking } from "./features/booking/data/booking.store";
+import { Booking } from "./features/booking/ui/booking/booking";
+
 export const App = () => {
-  return <h1>Hostfully</h1>;
+  const updateCurrentBooking = useUpdateCurrrentBooking();
+  const clearCurrentBooking = useClearCurrentBooking();
+  const [selectedProperty, setSelectedProperty] = useState<Property | null>(null);
+
+  const onPropertySelect = (property: Property) => {
+    updateCurrentBooking({ property_id: property.id });
+    setSelectedProperty(property);
+  };
+
+  const onBookingCancel = () => {
+    clearCurrentBooking();
+    setSelectedProperty(null);
+  };
+
+  return (
+    <main>
+      <Header title="Hostfully" />
+      <PropertiesList onPropertySelect={onPropertySelect} />
+      {selectedProperty?.id && <Booking property={selectedProperty} onBookingCancel={onBookingCancel} />}
+    </main>
+  );
 };
