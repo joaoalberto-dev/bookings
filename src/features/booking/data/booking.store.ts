4cad7bb6476408e1bca148647c5c387b7118befb
João Alberto <joaoalberto.contato@gmail.com>
2026-01-16 09:25:49 -0300

    chore(bookings): creating new hooks for booking creation

--- src/features/booking/data/booking.store.ts
+++ src/features/booking/data/booking.store.ts
@@ -1,3 +1,4 @@
+import { differenceInDays, parseISO } from "date-fns";
 import { create } from "zustand";
 
 import { type Booking } from "./booking.types";
@@ -7,6 +8,7 @@ interface BookingState {
   currentBooking: Partial<Booking> | null;
   error: string | null;
   updateCurrentBooking: (booking: Partial<Booking> | null) => void;
+  clearCurrentBooking: () => void;
   createBooking: (booking: Booking) => void;
 }
 
@@ -22,6 +24,9 @@ const useBookingStore = create<BookingState>((set, get) => ({
       },
     });
   },
+  clearCurrentBooking() {
+    set({ currentBooking: null });
+  },
   createBooking(booking: Booking) {
     set({
       bookings: [...get().bookings, booking],
@@ -31,4 +36,15 @@ const useBookingStore = create<BookingState>((set, get) => ({
 
 export const useBookings = () => useBookingStore((store) => store.bookings);
 export const useCurrentBooking = () => useBookingStore((store) => store.currentBooking);
-export const useUpdateBooking = () => useBookingStore((store) => store.updateCurrentBooking);
+export const useCreateBooking = () => useBookingStore((store) => store.createBooking);
+export const useUpdateCurrrentBooking = () => useBookingStore((store) => store.updateCurrentBooking);
+export const useClearCurrentBooking = () => useBookingStore((store) => store.clearCurrentBooking);
+export const useBookingNights = () =>
+  useBookingStore((store) => {
+    const start = store.currentBooking?.start_date;
+    const end = store.currentBooking?.end_date;
+
+    if (!start || !end) return 1;
+
+    return differenceInDays(parseISO(end), parseISO(start));
+  });
